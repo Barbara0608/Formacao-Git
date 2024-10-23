f029a1554755023fe5bd3a7de014aca4b53320e4
Francisco Simiao <francisco.p.simiao@gmail.com>
2024-10-23 18:57:17 +0100

    Francisco Simiao

--- Formacao_Git.py
+++ Formacao_Git.py
@@ -1,11 +1,12 @@
 def main():
     print("Hello World")
-    
+
     print("Hello Junitecos!")
     print("Hi, Bárbara!")
     print("Hi, Henrique Machado")
     print("Hiiiii, Ema")
+    print("HI, Francisco Simiao")
+
 
 if __name__ == "__main__":
     main()
-
