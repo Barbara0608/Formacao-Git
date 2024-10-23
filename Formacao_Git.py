8dbb41818dcdf1374516bbce929308d7a53542ca
jaimevidex <jo.silva@campus.fct.unl.pt>
2024-10-23 18:52:50 +0100

    branch-jaime-publish

--- Formacao_Git.py
+++ Formacao_Git.py
@@ -4,6 +4,11 @@ def main():
     print("Hello Junitecos!")
     print("Hi, Bárbara!")
     print("Hi, Henrique Machado")
+<<<<<<< Updated upstream
+=======
+    print("Hiiiii, Ema")
+    print("Hi, Jaime Silva")
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
     main()
